fe1e15fb8d5b003372260c7e2fe865d17d00ee89
Steven Morrison <8107863+Zaffri@users.noreply.github.com>
2025-11-10 22:33:36 +0000

    Finish bit packing function

--- main.go
+++ main.go
@@ -6,26 +6,35 @@ import (
 )
 
 func main() {
-	// bools := []bool{true, false, true, false, false, false, false, false}
-	// packedBools, err := packBoolArray(bools)
+	bools := []bool{true, false, true, false, false, false, false, false}
+	fmt.Printf("Raw: %v \n\n", bools)
+	packedBools, err := packBoolArray(bools)
 
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// } else {
-	// 	fmt.Printf("Packed bools: %v \n\n", packedBools)
-	// }
-	byteTest(1)
-	byteTest(7)
-	byteTest(10)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println("Packed")
+		printByte(packedBools)
+	}
 }
 
 func packBoolArray(unpackedBools []bool) (packedBools byte, err error) {
 	if len(unpackedBools) > 8 {
-		return 0, errors.New("There is a max size of 8 booleans")
+		return 0, errors.New("There is a max size of 8 booleans for simplicity")
 	}
-	fmt.Printf("Unpacked: %v \n\n", unpackedBools)
 
+	for index, bool := range unpackedBools {
+		if bool {
+			mask := byte(1 << index)
+			packedBools = packedBools | mask
+		}
+	}
 	return packedBools, nil
+}
+
+func printByte(byteToPrint byte) {
+	fmt.Printf("Decimal: %d\n", byteToPrint)
+	fmt.Printf("Binary: %08b\n\n", byteToPrint)
 }
 
 func byteTest(shiftLeft int) {
@@ -34,6 +43,5 @@ func byteTest(shiftLeft int) {
 	mask := byte(1 << shiftLeft)
 	myWeeByte = myWeeByte | mask
 
-	fmt.Printf("Decimal: %d\n", myWeeByte)
-	fmt.Printf("Binary: %08b\n\n", myWeeByte)
+	printByte(myWeeByte)
 }
